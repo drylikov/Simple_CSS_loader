4d1faa415acc2a8e53fbfdcceb21ace6f9eff423
drylikov <denis.rylikov@protonmail.com>
2025-11-02 13:18:17 +0300

    Simple_CSS_loader
    Lightweight CSS loader for Webpack .

---
+++ index.js
@@ -0,0 +1,16 @@
+
+module.exports = function(css) {
+    this.cacheable && this.cacheable();
+
+    return "module.exports = (function(d) {\n\
+        var head = d.head || d.getElementsByTagName('head')[0];\n\
+        if (head) {\n\
+            var style = d.createElement('style');\n\
+            style.type = 'text/css';\n\
+            style.textContent = " + JSON.stringify(css) + ";\n\
+            head.appendChild(style);\n\
+            return style;\n\
+        }\n\
+        return null;\n\
+    })(document);\n";
+};
